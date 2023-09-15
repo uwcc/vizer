1a3fddc19591942badb463ff964941eea0afd023
Phoebe Zeller <43123953+uwcc@users.noreply.github.com>
2023-09-15 15:00:42 +1200

    Made starter simple, and counter fancy
    The words fade out code is too fancy for students to start with, so I paired it with the bar style that uses maps.  This way starter is as simple as

--- music_counter.js
+++ music_counter.js
@@ -1,34 +1,53 @@
 
+let lastWords = "...";
+let wordBrightness = 255;
+let yOffset = 0;
+
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
-  let volume_vocal = map(vocal, 0, 100, 0, 0.7 * height, true);
-  let volume_drum  = map(drum, 0, 100, 0, 0.7 * height, true);
-  let volume_bass  = map(bass, 0, 100, 0, 0.7 * height, true);
-  let volume_other = map(other, 0, 100, 0, 0.7 * height, true);
+background(0);
+rectMode(CENTER);
+textAlign(CENTER);
+textFont('Helvetica'); // please use CSS safe fonts
 
-
-  background(20);
-  rectMode(CENTER);
-
-  fill(200, 0, 0);
-  rect(2*width/10, height/2, width/6, volume_vocal);
-  fill(0, 200, 0);
-  rect(4*width/10, height/2, width/6, volume_drum);
-  fill(200, 200, 200);
-  rect(6*width/10, height/2, width/6, volume_bass);
-  fill(0, 0, 200);
-  rect(8*width/10, height/2, width/6, volume_other);
-
-  fill(255, 255, 0);
-
-  textAlign(LEFT);
-  // demonstrate use of non-documented "counter" variable
-  let seconds = counter/60
-  if(seconds > 0) {
-    textSize(60);
-    text(nf(seconds, 3, 2), 20, height-20);
+if (words == "") {
+  wordBrightness = int(wordBrightness * 0.95); //fade brightness 
+  words = lastWords; // safe the last known word so we can display 
+  if (yOffset < height / 4) {
+    yOffset = yOffset + 1;
   }
+} else {
+  wordBrightness = 255; // set brightness to max
+  yOffset = 0; //don't offset down
+  lastWords = words; // keep track of what the most recent word is
+}
 
-  textAlign(CENTER);
-  textSize(vocal);
-  text(words, width/2, height/8);
+fill(wordBrightness);
+textSize(40);
+text(words, width / 2, height / 5 + yOffset); // display current lyric in middle of page, then fade down
+
+let volume_vocal = map(vocal, 0, 100, 0, 0.7 * height, true);
+let volume_drum  = map(drum, 0, 100, 0, 0.7 * height, true);
+let volume_bass  = map(bass, 0, 100, 0, 0.7 * height, true);
+let volume_other = map(other, 0, 100, 0, 0.7 * height, true);
+
+
+rectMode(CENTER);
+let rectY = height/3 *2
+fill(200, 0, 0);
+rect(2*width/10, rectY, width/6, volume_vocal);
+fill(0, 200, 0);
+rect(4*width/10, rectY, width/6, volume_drum);
+fill(200, 200, 200);
+rect(6*width/10, rectY, width/6, volume_bass);
+fill(0, 0, 200);
+rect(8*width/10, rectY, width/6, volume_other);
+
+fill(200)
+textAlign(LEFT);
+// demonstrate use of non-documented "counter" variable
+let seconds = counter/60
+if(seconds > 0) {
+  textSize(60);
+  text(nf(seconds, 3, 2), 20, height-20);
+}
 }
--- music_starter.js
+++ music_starter.js
@@ -1,60 +1,43 @@
-let lastWords = "...";
-let wordBrightness = 255;
-let yOffset = 0;
 
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
-function draw_one_frame(words, vocal, drum, bass, other) {
- // background(0);
-  rectMode(CENTER);
-  textAlign(CENTER);
+function draw_one_frame(words, vocal, drum, bass, other, counter) {
+  background(20)
   textFont('Helvetica'); // please use CSS safe fonts
-
-
-console.log(words)
-  if (words == "") {
-    wordBrightness = int(wordBrightness * 0.95);
-    words = lastWords;
-    if (yOffset < height / 4) {
-      yOffset = yOffset + 1;
-    }
-  } else {
-    wordBrightness = 255;
-    yOffset = 0;
-    lastWords = words;
-  }
-
-  fill(wordBrightness);
-  textSize(40);
-  text(words, width / 2, height / 5 + yOffset);
  
-
-
-  let bar_spacing = height / 10;
-  let bar_height = width / 12;
-  let bar_pos_x = width / 2;
-  textSize(24);
-
-  // vocal bar is red
-  fill(200, 0, 0);
-  rect(bar_pos_x, height / 2 + 1 * bar_spacing, 4 * vocal, bar_height);
-  fill(0);
-  text("vocals", bar_pos_x, height / 2 + 1 * bar_spacing + 8);
-
-  // drum bar is green
-  fill(0, 200, 0);
-  rect(bar_pos_x, height / 2 + 2 * bar_spacing, 4 * drum, bar_height);
-  fill(0);
-  text("drums", bar_pos_x, height / 2 + 2 * bar_spacing + 8);
-
-  // bass bar is blue
-  fill(50, 50, 240);
-  rect(bar_pos_x, height / 2 + 3 * bar_spacing, 4 * bass, bar_height);
-  fill(0);
-  text("bass", bar_pos_x, height / 2 + 3 * bar_spacing + 8);
-
-  // other bar is white
-  fill(200, 200, 200);
-  rect(bar_pos_x, height / 2 + 4 * bar_spacing, 4 * other, bar_height);
-  fill(0);
-  text("other", bar_pos_x, height / 2 + 4 * bar_spacing + 8);
+  rectMode(CENTER)
+   let bar_spacing = height / 10;
+   let bar_height = width / 12;
+   let bar_pos_x = width / 2;
+ 
+   textSize(24);
+   // vocal bar is red
+   fill(200, 0, 0);
+   rect(bar_pos_x, height / 2 + 1 * bar_spacing, 4 * vocal, bar_height);
+   fill(0);
+   text("vocals", bar_pos_x, height / 2 + 1 * bar_spacing + 8);
+ 
+   // drum bar is green
+   fill(0, 200, 0);
+   rect(bar_pos_x, height / 2 + 2 * bar_spacing, 4 * drum, bar_height);
+   fill(0);
+   text("drums", bar_pos_x, height / 2 + 2 * bar_spacing + 8);
+ 
+   // bass bar is blue
+   fill(50, 50, 240);
+   rect(bar_pos_x, height / 2 + 3 * bar_spacing, 4 * bass, bar_height);
+   fill(0);
+   text("bass", bar_pos_x, height / 2 + 3 * bar_spacing + 8);
+ 
+   // other bar is white
+   fill(200, 200, 200);
+   rect(bar_pos_x, height / 2 + 4 * bar_spacing, 4 * other, bar_height);
+   fill(0);
+   text("other", bar_pos_x, height / 2 + 4 * bar_spacing + 8);
+   fill(255, 255, 0);
+ 
+ 
+ 
+   textAlign(CENTER);
+   textSize(vocal);
+   text(words, width/2, height/3);
 }
