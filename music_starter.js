bfee5b7888ddf3349b3c6361b6839a0bba63de83
Phoebe Zeller <43123953+uwcc@users.noreply.github.com>
2023-09-15 15:36:08 +1200

    starter tidy

--- music_starter.js
+++ music_starter.js
@@ -3,13 +3,14 @@
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
   background(20)
   textFont('Helvetica'); // please use CSS safe fonts
- 
   rectMode(CENTER)
+  textSize(24);
+
    let bar_spacing = height / 10;
    let bar_height = width / 12;
    let bar_pos_x = width / 2;
  
-   textSize(24);
+
    // vocal bar is red
    fill(200, 0, 0);
    rect(bar_pos_x, height / 2 + 1 * bar_spacing, 4 * vocal, bar_height);
@@ -34,8 +35,6 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
    fill(0);
    text("other", bar_pos_x, height / 2 + 4 * bar_spacing + 8);
    fill(255, 255, 0);
- 
- 
  
    textAlign(CENTER);
    textSize(vocal);
