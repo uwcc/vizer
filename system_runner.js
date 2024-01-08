dc50a18ec41426c4f260fc576f29171057d4f00e
Phoebe Zeller <43123953+uwcc@users.noreply.github.com>
2024-01-09 10:48:20 +1300

    updated counter to return song.currentTime()

--- system_runner.js
+++ system_runner.js
@@ -202,7 +202,8 @@ function draw() {
         slider2.value(row[1]);
         slider3.value(row[2]);
         slider4.value(row[3]);
-        draw_one_frame(cur_words, row[0], row[1], row[2], row[3], curSlice);
+       // draw_one_frame(cur_words, row[0], row[1], row[2], row[3], curSlice);currentTime()
+       draw_one_frame(cur_words, row[0], row[1], row[2], row[3], song.currentTime());
       }
     }
   }
